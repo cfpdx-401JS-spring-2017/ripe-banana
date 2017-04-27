84f7beb8986bcc117c73efc96488a7bafa207a70
Morgan Fogarty <mofo37@gmail.com>
2017-04-27 15:52:45 -0700

    Made route test pass

---
+++ test/e2e/studios.test.js
@@ -0,0 +1,14 @@
+const db = require('./_db');
+const request = require('./_request');
+const assert = require('chai').assert;
+
+describe('studios API', () => {
+  before(db.drop);
+  it('GETs all studios', () => {
+    return request.get('/api/studios')
+      .then(res => {
+        const studios = res.body;
+        assert.deepEqual(studios, []);
+      });
+  });
+});
