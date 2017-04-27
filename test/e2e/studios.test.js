2bf63620efdecb4f137b12f0608e6d1dad9c0fbd
Morgan Fogarty <mofo37@gmail.com>
2017-04-27 16:38:12 -0700

    get by id test passes

--- test/e2e/studios.test.js
+++ test/e2e/studios.test.js
@@ -11,4 +11,43 @@ describe('studios API', () => {
         assert.deepEqual(studios, []);
       });
   });
+
+  let fakeStudio = {
+    name: 'Paramount',
+    address: {
+      city: 'LA',
+      state: 'California',
+      country: 'USA'
+    }
+  };
+
+  let fakeStudio2 = {
+    name: 'Disney',
+    address: {
+      city: 'Burbank',
+      state: 'California',
+      country: 'USA'
+    }
+  };
+
+  function savedStudio(studio) {
+    return request.post('/api/studios')
+      .send(studio)
+      .then(res => res.body);
+  }
+
+  it('roundtrips a studio', () => {
+    return savedStudio(fakeStudio)
+      .then(saved => {
+        assert.ok(saved._id, 'saved has id');
+        fakeStudio = saved;
+      })
+      .then(() => {
+        return request.get(`/api/studios/${fakeStudio._id}`);
+      })
+      .then(res => {
+        const studio = res.body;
+        assert.deepEqual(studio, fakeStudio);
+      });
+  });
 });
