ef86923065b8408a688c45b84e763b26a7be4549
Keeley Hammond <keeleymhammond@gmail.com>
2017-04-27 16:38:06 -0700

    roundtrip function is working

--- test/e2e/studios.test.js
+++ test/e2e/studios.test.js
@@ -8,10 +8,49 @@ describe('Studios API', () => {
 
   it('GET / all studios', () => {
     return request.get('/api/studios')
-    .then(res => {
-      const studios = res.body;
-      assert.deepEqual(studios, []);
-    });
+      .then(res => {
+        const studios = res.body;
+        assert.deepEqual(studios, []);
+      });
+  });
+
+  let paramount = {
+    name: 'Paramount',
+    address: {
+      city: 'Los Angeles',
+      state: 'CA',
+      country: 'USA'
+    }
+  };
+
+  let laika = {
+    name: 'Laika',
+    address: {
+      city: 'Portland',
+      state: 'OR',
+      country: 'USA'
+    }
+  };
+
+  function saveStudio(studio) {
+    return request.post('/api/studios')
+      .send(studio)
+      .then(res => res.body);
+  }
+
+  it('roundtrips a studio', () => {
+    return saveStudio(paramount)
+      .then(saved => {
+        assert.ok(saved._id, 'saved has id');
+        paramount = saved;
+      })
+      .then(() => {
+        return request.get(`/api/studios/${paramount._id}`);
+      })
+      .then(res => res.body)
+      .then(studio => {
+        assert.deepEqual(studio, paramount);
+      });
   });
 
 });
