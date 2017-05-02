acdef81a4e91d2ad353154c0cdd39f22f7ee5236
Chris_Wallace <kawallis@icloud.com>
2017-05-02 12:59:23 -0700

    changes

--- test/e2e/studios.test.js
+++ test/e2e/studios.test.js
@@ -1,3 +1,4 @@
+
 const db = require('./_db');
 const request = require('./_request');
 const assert = require('chai').assert;
@@ -5,11 +6,11 @@ const assert = require('chai').assert;
 describe('Studios api', () => {
     before(db.drop);
 
-    it('initial /GET returns empty list', () => {
+    it('initial /GET returns one studio list', () => {
         return request.get('/api/studios')
             .then(req => {
                 const studios = req.body;
-                assert.deepEqual(studios, []);
+                assert.deepEqual(studios.length, 1);
             });
     });
 
@@ -41,33 +42,32 @@ describe('Studios api', () => {
     };
 
     function saveStudio(studio) {
-        return request
-            // post our new musician    
+        return request   
             .post('/api/studios')
-            // send the data as the request body
             .send(studio)
-            .then(res => res.body);
+            .then(res => studio = res.body);
     }
 
-    it('roundtrips a new Studio', () => {
-        return saveStudio(summerTime)
-            .then(saved => {
-                // check that we were assigned id
-                assert.ok(saved._id, 'saved has id');
-                // reassign saved version to our variable
-                summerTime = saved;
-            })
-            // go get this same pet by id
-            .then(() => {
-                return request.get(`/api/studios/${summerTime._id}`);
-            })
-            // get the data (pet) off they response body
-            .then(res => res.body)
-            .then(got => {
-                // should be same as response from post
-                assert.deepEqual(got, summerTime);
-            });
-    });
+    // it('roundtrips a new Studio', () => {
+    //     return saveStudio(summerTime)
+    //         .then(saved => {
+    //             // check that we were assigned id
+    //             assert.ok(saved._id, 'saved has id');
+    //             // reassign saved version to our variable
+    //             summerTime = saved;
+    //         })
+    //         // go get this same pet by id
+    //         .then(() => {
+    //             return request.get(`/api/studios/${summerTime._id}`);
+    //         })
+    //         // get the data (pet) off they response body
+    //         .then(res => res.body)
+    //         .then(got => {
+                
+    //             // should be same as response from post
+    //             assert.deepEqual(got, summerTime);
+    //         });
+    // });
 
     it('GET returns 404 for non-existent id', () => {
         const nonId = '589d04a8b6695bbdfd3106f1';
@@ -80,19 +80,81 @@ describe('Studios api', () => {
             );
     });
 
+    let studio1 = {
+        name: 'SS Studios',
+        address: {
+            city: 'Pasadena' ,
+            state: 'CA',
+            country: 'USA'
+        }
+    };
+
+    before( () => {   
+        return request.post('/api/studios')
+            .send(studio1)
+            .then (res => studio1 = res.body);
+    });
+
+    let film = null;
+    before( () => {
+        film = {
+            title: 'O Brother',
+            studio: studio1._id,
+            released: '08/23/1989',
+            cast: []
+        };
+        return request.post('/api/films')
+            .send(film)
+            .then (res => film = res.body);
+    });
+
+    let filmTwo = null;
+    before( () => {
+        filmTwo = {
+            title: 'My Hitta my Hitta',
+            studio: studio1._id,
+            released: '08/23/1989',
+            cast: []
+        };
+        return request.post('/api/films')
+            .send(filmTwo)
+            .then (res => filmTwo = res.body);
+    });
+
+    it('tests for the /:id in the Studio', () => {
+        return request.get(`/api/studios/${studio1._id}`)
+            .then(res => {
+                studio1 = res.body;
+                let obj = {
+                    __v: 0,
+                    _id: studio1._id,
+                    name: 'SS Studios',
+                    address: {
+                        city: 'Pasadena',
+                        state: 'CA',
+                        country: 'USA'
+                    },
+                    films: [film.title, filmTwo.title]
+                };
+                assert.deepEqual(studio1, obj);
+            });
+    });
+
     it('returns list of all studios', () => {
         return Promise.all([
             saveStudio(garfield),
+            saveStudio(summerTime),
             saveStudio(warnerbros)
         ])
             .then(savedStudios => {
                 garfield = savedStudios[0];
                 warnerbros = savedStudios[1];
+                summerTime = savedStudios[2];
             })
             .then(() => request.get('/api/studios'))
             .then(res => res.body)
             .then(studios => {
-                assert.equal(studios.length, 3);
+                assert.equal(studios.length, 4);
                 assert.include(studios, summerTime);
                 assert.include(studios, garfield);
                 assert.include(studios, warnerbros);
@@ -119,7 +181,7 @@ describe('Studios api', () => {
             .then(() => request.get('/api/studios'))
             .then(res => res.body)
             .then(studios => {
-                assert.equal(studios.length, 2);
+                assert.equal(studios.length, 3);
             });
     });
 
