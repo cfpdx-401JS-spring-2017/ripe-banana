70d69c6371a028abaddef9be2ab82d23dae761e3
Keeley Hammond <keeleymhammond@gmail.com>
2017-05-01 19:58:06 -0700

    Updated get all and update studio methods

--- test/e2e/studios.test.js
+++ test/e2e/studios.test.js
@@ -53,13 +53,44 @@ describe('Studios API', () => {
       });
   });
 
-  it('returns 404 if the coffee does not exist', () => {
+  it('returns 404 if the studio does not exist', () => {
     const notId = '589d04a8b6695bbdfd3106f1';
-    return request.get(`/api/coffees/${notId}`)
+    return request.get(`/api/studios/${notId}`)
       .then(() => { throw new Error('expected 404'); },
       res => {
         assert.equal(res.status, '404');
       });
+  });
+
+  it('returns list of all studios', () => {
+    return Promise.all([
+      saveStudio(laika)
+    ])
+      .then(savedStudios => {
+        laika = savedStudios[0];
+      })
+      .then(() => request.get('/api/studios'))
+      .then(res => res.body)
+      .then(studios => {
+        assert.equal(studios.length, 2);
+        assert.include(studios, paramount);
+        assert.include(studios, laika);
+      });
+  });
+
+  it('updates a studio', () => {
+    laika.name = 'Stoopid Buddy';
+    return request.put(`/api/studios/${laika._id}`)
+      .send(laika)
+      .then(res => res.body)
+      .then(updated => {
+        assert.equal(updated.name, 'Stoopid Buddy');
+      });
+
+  });
+
+  it('removes a studio', () => {
+    //
   });
 
 });
