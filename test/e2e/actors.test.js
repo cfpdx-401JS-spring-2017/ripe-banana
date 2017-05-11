5d7b1aec0363b3dfaa07c7fcefb4de5c8d7a3166
Morgan Fogarty <mofo37@gmail.com>
2017-05-10 22:31:05 -0700

    add actor routes. roundtripping an actor passing.

--- test/e2e/actors.test.js
+++ test/e2e/actors.test.js
@@ -14,5 +14,43 @@ describe('actors API', () => {
       });
   });
 
+  let fakeActor = {
+    name: 'Lief Garret',
+    dob: 1974
+  };
 
+  function savedActor(actor) {
+    return request
+      .post('/api/actors')
+      .send(actor)
+      .then(res => res.body);
+  }
+
+  it('roundtrips an actor', () => {
+    return savedActor(fakeActor)
+      .then(savedActor => {
+        assert.ok(savedActor._id, 'saved has id');
+        fakeActor = savedActor;
+      })
+      .then(() => {
+        return request.get(`/api/actors/${fakeActor._id}`);
+      })
+      .then(res => res.body)
+      .then(got => {
+        assert.deepEqual(got, fakeActor);
+      });
+  });
+
+  it('deletes an actor', () => {
+    return request.delete(`/api/actors/${fakeActor._id}`)
+      .then(res => res.body)
+      .then(result => {
+        assert.isTrue(result.removed);
+      })
+      .then(() => request.get('/api/actors'))
+      .then(res => res.body)
+      .then(actors => {
+        assert.equal(actors.length, 0);
+      });
+  });
 });
